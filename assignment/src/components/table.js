dd60b41e102a670a80976ec4dc62420177b036e2
nehakhatriya <nehakhatriya@gmail.com>
2020-09-04 14:20:06 +0530

    table with display and delete functionality

---
+++ assignment/src/components/table.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import EmployeeTableContent from './tableStructure'
+class EmployeeTable extends Component {
+
+    state = { employees: [{ name: "neha", job: "manager" }, { name: "nikita", job: "trainee" }, { name: "jai", job: "consultant" }] }
+    deleteEmployee(event, index) {
+        const emp = this.state.employees.slice()
+        emp.splice(index, 1)
+        this.setState({ employees: emp })
+    }
+
+    render() {
+        let employees = (
+
+            this.state.employees.map((employee, index) => {
+                return <EmployeeTableContent name={employee.name} job={employee.job}
+                    click={(event) => this.deleteEmployee(event, index)} />
+            })
+        )
+        return (
+            < div >
+                <div className="container">
+                    <h4> Employee Table</h4>
+                    <table className="table">
+                        <tr>
+                            <th scope="col">Name</th>
+                            <th scope="col">Job</th>
+                            <th scope="col">Remove</th>
+                        </tr>
+                        {employees}
+                    </table>
+                </div>
+            </div >
+        )
+    }
+}
+export default EmployeeTable;

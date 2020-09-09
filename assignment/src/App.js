230dcc8ac572721909f4eca1809e8bf10d2862d5
nehakhatriya <nehakhatriya@gmail.com>
2020-09-09 21:14:56 +0530

    added calc

--- assignment/src/App.js
+++ assignment/src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import EmployeeTable from './components/table';
 import './App.css';
+import Calculator from './components/calculator/calculator'
 
 function App() {
   return (
-    <div className="App">
-      
+    <Fragment>
+    <div className="Employee"> 
      <EmployeeTable></EmployeeTable>
-     
     </div>
+    <div className="Calculator"> 
+     <Calculator></Calculator>
+    </div>
+    </Fragment>
   );
 }
 
